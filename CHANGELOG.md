6ac2c46c6023efd87f02971f5ec6d68f7c733d10
Petr Skoda <petr@mutms.org>
2026-09-18 11:53:03 +0200

    Moodle 5.3dev migration

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,14 +4,4 @@ Plugin versioning is derived from Moodle releases, it does not comply with the s
 
 The format of this change log follows the advice given at [Keep a CHANGELOG](https://keepachangelog.com).
 
-## [Unreleased](https://github.com/mutms/moodle-tool_mucatalog/compare/v5.0.6.00...MOODLE_500_STABLE)
-
-### Added
-
-- Added composer.json for Packagist distribution
-
-## [v5.0.6.00](https://github.com/mutms/moodle-tool_mucatalog/tree/v5.0.6.00) - 2026-03-18
-
-### Added
-
-- Initial release
+## [Unreleased]
--- version.php
+++ version.php
@@ -28,12 +28,12 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_mucatalog';
-$plugin->version = 2026031850;
-$plugin->requires = 2024100700;
-$plugin->maturity = MATURITY_ALPHA;
-$plugin->supported = [500, 501];
-$plugin->release = 'v5.0.6.00+';
+$plugin->version = 2026091953;
+$plugin->requires = 2026091600;
+
+$plugin->supported = [503, 503];
+$plugin->release = 'v5.3.0.00';
 
 $plugin->dependencies = [
-    'tool_mulib' => 2026030150,
+    'tool_mulib' => 2026091953,
 ];
